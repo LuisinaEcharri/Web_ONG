70562f120744d3075a3b7fe3d82e1679b11955d9
LuisinaEcharri <luisina_echarri@hotmail.com>
2024-05-03 23:10:04 -0300

    agregada bdd

---
+++ bdd.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+// Configuración de la conexión a la base de datos
+const connection = mysql.createConnection({
+  host: 'localhost', 
+  user: 'root',
+  password: 'contramysql',
+  database: 'inscriptos'
+});
+
+// Establecer la conexión
+connection.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos: ', err);
+    return;
+  }
+  console.log('Conexión exitosa a la base de datos MySQL');
+});
+
--- dockerfile
+++ dockerfile
@@ -1,2 +1,5 @@
-FROM nginx:latest
+# Usa una imagen base de Nginx ligera
+FROM nginx:alpine
+
+# Copia tu código de la página web al directorio de trabajo de Nginx
 COPY . /usr/share/nginx/html
